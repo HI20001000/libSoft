dc71e9af0c634db5723bfa5ea4aaad98689124bb
HI20001000 <thousandsnow01@gmail.com>
2023-11-22 18:49:25 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Description: 
+ * @Author: Rabbiter
+ * @Date: 2023-02-21 18:13:21
+-->
+<template>
+    <div id="app">
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    mounted() {},
+};
+</script>
